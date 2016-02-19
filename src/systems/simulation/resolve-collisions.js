b26aeaaeaca6fb765833daec7ea0f6bed3b9d0c0
Zach Bouvier <thebouvse@gmail.com>
2016-02-19 16:14:17 -0500

    Fixed Indentation in for resolve-collusions.js

--- src/systems/simulation/resolve-collisions.js
+++ src/systems/simulation/resolve-collisions.js
@@ -5,21 +5,23 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 		var entitiesCollision = game.entities.find("collisions");
 		var entitiesObstacle = game.entities.find("obstacle");
 		var foundCollisions = false;
-		var foundObstacle = true;
+		var foundObstacle = false;
 		for (var i = 0; i < entitiesCollision.length; i++) {
 			var collisions = game.entities.get(entitiesCollision[i], "collisions");
 			if (collisions.length > 0) {
 				foundCollisions = true;
 				break;
-			};
+			}
+			if (obstacle.length > 0) {
+				foundObstacle = true;
+				break;
+			}
 		}
-	}
-	if (foundCollisions && foundObstacle) {
-		var entitiesWithVelocity = game.entities.find("velocity");
-		for (i = 0; i < entitiesWithVelocity.length; i++) {
-			game.entities.remove(entitiesWithVelocity[i], "velocity");
+		if (foundCollisions && foundObstacle) {
+			var entitiesWithVelocity = game.entities.find("velocity");
+			for (i = 0; i < entitiesWithVelocity.length; i++) {
+				game.entities.remove(entitiesWithVelocity[i], "velocity");
+			}
 		}
-	}
 	});
-
 };
