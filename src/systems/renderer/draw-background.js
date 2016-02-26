00a88393b983d45d99bd996544920cd23869ad81
Zach Bouvier <thebouvse@gmail.com>
2016-02-26 16:54:46 -0500

    Made it pertty

---
+++ src/systems/renderer/draw-background.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
+	ecs.addEach(function harvest(entity, context) { // eslint-disable-line no-unused-vars
+		var size = game.entities.get(entity, "size");
+		var color = game.entities.get(entity, "color");
+		var position = game.entities.get(entity, "position");
+		context.fillStyle = color;
+		context.fillRect(position.x, position.y, size.width, size.height);
+	} ,"background");
+};
