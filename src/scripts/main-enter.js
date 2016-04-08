3f2cc0c3942665b2529122c3282527a096b653b5
Zach Bouvier <thebouvse@gmail.com>
2016-04-08 15:51:59 -0400

    Started working on cookies

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,5 +1,12 @@
 "use strict";
 
+var player = 2;
+var saveData = require("splat-ecs/lib/save-data");
 module.exports = function(game) { // eslint-disable-line no-unused-vars
+	saveData.get("highscore", function(hs) {
+		game.entities.set(player, "highscore", hs);
+	});
 	game.scaleCanvasToFitRectangle(288,512);
+	var score = game.entities.get(player, "score");
+
 };
