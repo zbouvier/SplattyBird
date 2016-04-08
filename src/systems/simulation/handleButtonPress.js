98ecce6463d3020492205d2eba03983bd43bf11e
Zach Bouvier <thebouvse@gmail.com>
2016-04-08 09:58:44 -0400

    Removed frames, fixed button, fixed button handling

--- src/systems/simulation/handleButtonPress.js
+++ src/systems/simulation/handleButtonPress.js
@@ -1,21 +1,9 @@
 "use strict";
-function isMouseInside(game, viewPort, container) {
-	var position = game.entities.get(container, "position");
-	var size = game.entities.get(container, "size");
-	var mouse = game.inputs.mouse;
-	var viewPortPosition = game.entities.get(viewPort, "position");
-	var mouseX = mouse.x + viewPortPosition.x;
-	return mouseX >= position.x &&
-		mouseX <= position.x + size.width &&
-		mouse.y >= position.y &&
-		mouse.y <= position.y + size.height;
 
-}
-var background = 0;
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function handleButtonPress(entity, elapsed) { // eslint-disable-line no-unused-vars
 		var button = game.entities.get(entity, "button");
-		if ((game.inputs.buttonPressed("jump") || game.inputs.buttonPressed("restart")) && isMouseInside(game, background, entity)) {
+		if (game.inputs.buttonPressed("jump") || game.inputs.buttonPressed("restart")) {
 			var script = game.require(button.script);
 			script(entity, game);
 		}
